ab9f03740c8ed1c2cac94f621e062d505a8a884e
ahmed adel <ahmedadel123422@gmail.com>
2025-02-28 04:22:05 +0200

    commit

---
+++ lib/feature/auth/presentation/bloc/auth_bloc.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:sa7ety/feature/auth/presentation/bloc/auth_event.dart';
+import 'package:sa7ety/feature/auth/presentation/bloc/auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc() : super(AuthState()) {
+    on<AuthEvent>((event, emit) {});
+  }
+}
