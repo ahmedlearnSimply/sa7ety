8514678779456e191c78e29caefd8a881b8a312f
ahmed adel <ahmedadel123422@gmail.com>
2025-02-28 04:27:25 +0200

    commit

--- lib/feature/auth/presentation/bloc/auth_event.dart
+++ lib/feature/auth/presentation/bloc/auth_event.dart
@@ -1,1 +1,16 @@
+import 'package:sa7ety/core/enum/user_type_enum.dart';
+
 class AuthEvent {}
+
+class RegisterEvent extends AuthEvent {
+  final String name;
+  final String email;
+  final String password;
+  final UserType userType;
+  RegisterEvent({
+    required this.name,
+    required this.email,
+    required this.password,
+    required this.userType,
+  });
+}
--- lib/feature/auth/presentation/bloc/auth_state.dart
+++ lib/feature/auth/presentation/bloc/auth_state.dart
@@ -1,1 +1,12 @@
 class AuthState {}
+
+class AuthInital extends AuthState {}
+
+class RegisterLoadingState extends AuthState {}
+
+class RegisterSuccessState extends AuthState {}
+
+class AuthError extends AuthState {
+  final String message;
+  AuthError({required this.message});
+}
