b2e192e725b291bf7f63aa5299a7205c1e3debed
ahmed adel <ahmedadel123422@gmail.com>
2025-03-03 02:51:28 +0200

    commit

---
+++ lib/feature/auth/presentation/pages/doctor_register.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/widgets.dart';
+
+class DoctorRegister extends StatefulWidget {
+  const DoctorRegister({super.key});
+
+  @override
+  State<DoctorRegister> createState() => _DoctorRegisterState();
+}
+
+class _DoctorRegisterState extends State<DoctorRegister> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
