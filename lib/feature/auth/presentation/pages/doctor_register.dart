057d57dfebe9b19f87b2ebe3bcc76a18b75d9d07
ahmed adel <ahmedadel123422@gmail.com>
2025-03-04 04:02:18 +0200

    commit

--- lib/feature/auth/presentation/pages/doctor_register.dart
+++ lib/feature/auth/presentation/pages/doctor_register.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, prefer_final_fields
 
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 import 'package:intl/intl.dart';
 import 'package:sa7ety/core/constants/specialization.dart';
