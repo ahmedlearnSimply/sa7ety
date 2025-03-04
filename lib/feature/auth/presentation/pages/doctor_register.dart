43073352f4e397b07fc33305480fe39a818acc2d
ahmed adel <ahmedadel123422@gmail.com>
2025-03-04 02:02:46 +0200

    commit

--- lib/feature/auth/presentation/pages/doctor_register.dart
+++ lib/feature/auth/presentation/pages/doctor_register.dart
@@ -33,86 +33,98 @@ class _DoctorRegisterState extends State<DoctorRegister> {
           ),
         ),
       ),
-      body: Center(
-        child: Column(
-          children: [
-            Gap(20),
-            Stack(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: Column(
               children: [
-                CircleAvatar(
-                  radius: 60,
-                  backgroundImage: AssetImage(
-                    AppAssets.ahmed,
-                  ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        shape: BoxShape.circle,
-                        boxShadow: [
-                          BoxShadow(
-                            blurRadius: 10,
-                            color: Colors.black26,
-                          ),
-                        ]),
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Icon(
-                        Icons.camera_alt,
-                        color: Color.fromARGB(255, 0, 130, 236),
-                        size: 20,
+                Gap(20),
+                Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 60,
+                      backgroundImage: AssetImage(
+                        AppAssets.ahmed,
                       ),
                     ),
+                    Positioned(
+                      bottom: 0,
+                      right: 0,
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                            boxShadow: [
+                              BoxShadow(
+                                blurRadius: 10,
+                                color: Colors.black26,
+                              ),
+                            ]),
+                        child: Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: Icon(
+                            Icons.camera_alt,
+                            color: Color.fromARGB(255, 0, 130, 236),
+                            size: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                //*specialization
+                Padding(
+                  padding: const EdgeInsets.only(right: 10.0),
+                  child: Row(
+                    children: [
+                      Text(
+                        "التخصص",
+                        style: getBodyStyle(
+                          color: Colors.black,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
                   ),
+                ),
+                Gap(10),
+
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 5),
+                  decoration: BoxDecoration(
+                    color: AppColors.accentColor,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  child: DropdownButton(
+                      isExpanded: true,
+                      iconEnabledColor: AppColors.primary,
+                      icon: Icon(
+                        Icons.expand_circle_down_outlined,
+                      ),
+                      value: _specialization,
+                      onChanged: (String? newValue) {
+                        setState(
+                          () {
+                            _specialization = newValue ?? specialization[0];
+                          },
+                        );
+                      },
+                      items: specialization.map((element) {
+                        return DropdownMenuItem(
+                          value: element,
+                          child: Text(
+                            element,
+                            style: getBodyStyle(
+                              fontSize: 24,
+                            ),
+                          ),
+                        );
+                      }).toList()),
                 )
               ],
             ),
-
-            //*specialization
-            Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Row(
-                children: [
-                  Text(
-                    "التخصص",
-                    style: getBodyStyle(
-                      color: Colors.black,
-                      fontSize: 18,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-            Gap(20),
-            Padding(
-              padding:
-                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: AppColors.accentColor,
-                ),
-                child: DropdownButton(
-                    value: _specialization,
-                    onChanged: (String? newValue) {
-                      setState(
-                        () {
-                          _specialization = newValue ?? specialization[0];
-                        },
-                      );
-                    },
-                    items: specialization.map((element) {
-                      return DropdownMenuItem(
-                        value: element,
-                        child: Text(element),
-                      );
-                    }).toList()),
-              ),
-            )
-          ],
+          ),
         ),
       ),
     );
